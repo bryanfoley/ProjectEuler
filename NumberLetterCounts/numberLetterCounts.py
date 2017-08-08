945bfb6cc902d0980b4357f739cfde306d8be7fa
bryanfoley <shagoff@gmail.com>
2017-08-08 22:22:49 +0200

    Modified some bugs, still not correct

--- NumberLetterCounts/numberLetterCounts.py
+++ NumberLetterCounts/numberLetterCounts.py
@@ -1,5 +1,5 @@
 primeElements = {'0':0,'1':3,'2':3,'3':5,'4':4,'5':4,'6':3,'7':5,'8':5,'9':4,'10':3,'11':6
-,'12':6,'13':8,'14':8,'15':7,'teen':4,'ty':2,'20':6,'30':6,'40':5,'50':5,'60':5,'70':7,'80':7,'90':6,'00':7,'and':3,'000':8}
+,'12':6,'13':8,'14':8,'15':7,'16':7,'17':9,'18':8,'19':7,'teen':4,'ty':2,'20':6,'30':6,'40':5,'50':5,'60':5,'70':7,'80':6,'90':6,'00':7,'and':3,'000':8}
 
 sum = 0
 for num in range(1,1001):
@@ -7,10 +7,6 @@ for num in range(1,1001):
     	value = primeElements[str(num)]
         sum+= value
     except:
-        if 15 < num < 20:
-            coeff = 1
-            value = primeElements[str(num%10**coeff)] + primeElements['teen']
-            sum+=value
         if 20 <= num < 100:
             coeff = 1
             value = primeElements[str((num/10**coeff)*10**coeff)] + primeElements[str(num%10**coeff)]
@@ -20,7 +16,13 @@ for num in range(1,1001):
             if num%10**coeff == 0:
                 value = primeElements[str(num/10**coeff)] + primeElements['00']
             else:
-                value = primeElements[str(num/10**coeff)] + primeElements['00'] + primeElements['and'] + primeElements[str(((num%10**coeff)/10**(coeff-1))*(10**(coeff-1)))] + primeElements[str((num%10**coeff)%10**(coeff-1))]
+                value1 = primeElements[str(num/10**coeff)] + primeElements['00'] + primeElements['and'] 
+                if (num%10**coeff) > 20:
+                    value2 = primeElements[str(((num%10**coeff)/10**(coeff-1))*(10**(coeff-1)))] \
+                        + primeElements[str((num%10**coeff)%10**(coeff-1))]
+                else:
+                    value2 = primeElements[str(num%10**coeff)]
+                value = value1+value2
             sum+= value
         if num == 1000:
             value = primeElements['1'] + primeElements['000'] 
